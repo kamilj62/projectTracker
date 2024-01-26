c503538e68f5a7d5c936191aa1d8a8b462b937a6
Joseph Kamil <kamilj@umich.edu>
2024-01-25 21:43:22 -0800

    added updated project

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,5 +1,11 @@
 // Save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
+var tableEl = $('.table');
+var projectFormEl = $("#project-form");
+var projectNameEl = $('#project-name-input');
+var projectTypeEl = $('#project-type-input');
+var projectDateEL = $('#project-date-input');
+
 
 // Things to execute upon loading the page
 function init() {
@@ -16,13 +22,38 @@ init();
 // Handle displaying the time
 function displayTime() {
 	// TODO: use dayjs format method to display the current date and time
+	var now = dayjs()
+	timeDisplayEl.text(now);
 }
 
 // Get project data from local storage and display it
 function printProjectData() {
 	// TODO: clear current projects on the page
-	// TODO: get projects from localStorage
+	// TODO: get project from localStorage
+	var projectsSaved = localStorage.getItem('projects');
+
+	if (projectsSaved) {
+		projectsSaved = JSON.parse(projectsSaved);
+	}
+	else {
+		projectsSaved = [];
+	}
+
 	// TODO: loop through each project and create a table row for each project. Don't forget to append your new row to the table in DOM
+	for (let i = 0; i < projectsSaved.length; i++) {
+		var row = document.createElement("tr");
+		var tdName = document.createElement("td");
+		var tdType = document.createElement("td");
+		var tdDate = document.createElement("td");
+        tdName.textContent = projectsSaved[i].projectName;
+		tdType.textContent = projectsSaved[i].projectType;
+		tdDate.textContent = projectsSaved[i].projectDate;
+		row.append(tdName);
+		row.append(tdType);
+		row.append(tdDate);
+        tableEl.append(row);
+		
+	}
 }
 
 // Add a project to local storage and display the updated project data
@@ -31,17 +62,39 @@ function handleProjectFormSubmit(event) {
 
 	// TODO: read user input from the form and bundle it in a new project object
 
-	
+	var project = {
+		projectName: projectNameEl.val(),
+		projectType: projectTypeEl.val(),
+		projectDate: projectDateEL.val()
+	}
 
 	// TODO: retrieve the current array of projects from local storage, add the new project object to it and, save the updated array of projects to local storage
+	var projectsSaved = localStorage.getItem('projects');
+
+	if (projectsSaved) {
+		projectsSaved = JSON.parse(projectsSaved);
+	}
+	else {
+		projectsSaved = [];
+	}
+	
+	projectsSaved.push(project);
+
+	localStorage.setItem('projects', JSON.stringify(projectsSaved));
+
 
 	// TODO: re-render the saved project data
 
+	printProjectData();
 	// TODO: clear the form inputs
+		projectNameEl.val("");
+		projectTypeEl.val("");
+		projectDateEL.val("");
+
 }
 
 // TODO: Add a 'submit' event listener on the new project modal form
-
+projectFormEl.on('submit', handleProjectFormSubmit);
 // BONUS
 // Remove a project from local storage and display the updated project data
 function handleDeleteProject() {
